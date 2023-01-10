bcfbfcfdf6bf86ee232f639dab6998523c14d2de
natia <natia.chachkhiani1@gmail.com>
2023-01-10 09:28:56 -0800

    add git actions (#2)
    use working-directory
    add pytest-xdist
    add sample test to avoid exit 5 failure

--- api/api/db.py
+++ api/api/db.py
@@ -3,8 +3,9 @@ import os
 
 import sqlmodel as sqlm
 
-
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL", "postgresql://postgres:postgres@postgres:5432/postgres"
+)
 
 engine = sqlm.create_engine(DATABASE_URL, echo=True)
 
